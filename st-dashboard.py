dbe5a69f104b0d86533ea29c0f2b2c2f6cd4cae6
corous <70595494+corous@users.noreply.github.com>
2025-05-27 15:10:06 -0700

    v0.37.1
    Multi-agent final

--- st-dashboard.py
+++ st-dashboard.py
@@ -164,7 +164,7 @@ with left:
                 "PathLayer",
                 data=[{"path": p} for p in st.session_state["routes"]["drones"]],
                 get_path="path",
-                get_color=[255, 165, 0],
+                get_color=[128, 0, 128],
                 get_width=10,
             )
         )
